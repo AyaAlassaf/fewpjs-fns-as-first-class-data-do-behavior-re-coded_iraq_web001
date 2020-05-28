0038d09384933e6ebc7e5867eac9a568cc08008f
Aya Alassaf <ayafaisal242@yahoo.com>
2020-05-28 17:55:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function handleClick(e) {
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
 function greet(timeString){
-  let str=timeString.split();
-  let parsed=
+  let str=timeString.split(":");
+  let parsed=parseInt(str,2);
   if(hour < 12){
     return "Good Morning";
   }
